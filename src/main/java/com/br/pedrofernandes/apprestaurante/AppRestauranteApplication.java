da82260ff6d56f4b462e0fec4f3944c9f5ff23f1
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-01 13:44:38 -0300

    Primeiro commit

---
+++ src/main/java/com/br/pedrofernandes/apprestaurante/AppRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.br.pedrofernandes.apprestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestauranteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRestauranteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/pedrofernandes/apprestaurante/AppRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pedrofernandes.apprestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
